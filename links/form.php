00956d0933ba0e5872cf07e7b2329e438067190d
Okhotsk-hr <hrkiha183.service@gmail.com>
2026-03-28 00:08:32 +0900

    QRからのリンク

---
+++ links/form.php
@@ -0,0 +1,25 @@
+<?php
+include("db/form_connect.php");
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>第三回鉄道模型で遊ぼうアンケート</title>
+    <script>
+        window.onload = function() {
+            // URL
+            window.location.href = 'https://forms.gle/ua3uq7vQXo8tJTmy5';
+            // window.open('https://forms.gle/ua3uq7vQXo8tJTmy5', '_blank');
+        };
+    </script>
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ links/site.php
@@ -0,0 +1,26 @@
+<?php
+include("db/site_connect.php");
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>第三回鉄道模型で遊ぼう</title>
+    <script>
+        window.onload = function() {
+            // URL
+
+            window.location.href = 'http://engarumrhall.s322.xrea.com/event202603/';
+            // window.open('http://engarumrhall.s322.xrea.com/event202603/', '_blank');
+        };
+    </script>
+</head>
+
+<body>
+
+</body>
+
+</html>
